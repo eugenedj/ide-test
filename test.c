b321df87ed871c91b9655da91c28f802edb6f89f
eugenedj <duqisi@163.com>
2022-03-10 10:19:07 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
+#include <unistd.h>
+#include "pthread.h"
 
-//This is a comment.
-//second line
-// Third line
+int global_cnt = 0;
 
-int main()
+void my_print(int number)
 {
-  printf("Hello World!\n");
-  return 0;
+    printf("number is %d\n",number);
+}
+/*
+*这个是注释
+*/
+void *process(void * arg)
+{
+	int i,j;
+	printf("Starting process %s\n", (char *)arg);
+	j = 0;
+	for (i = 0; i < 10000; i++)
+	{
+		write(1, (char *) arg, 1);
+		sleep(1);
+		printf("test test test test\n");
+		global_cnt++;
+		j++;
+		printf("i = %d\n",i);
+		printf("j = %d\n",j);
+		my_print(i);
+	}
+	return NULL;
+}
+
+#define sucfail(r) (r != 0 ? "failed" : "succeeded")
+int main(void)
+{
+	int pret, ret = 0;
+	pthread_t th_a, th_b;
+	void *retval;
+
+	ret += (pret = pthread_create(&th_a, NULL, process, (void *)"a"));
+	printf("create a %s %d\n", sucfail(pret), pret);
+	ret += (pret = pthread_create(&th_b, NULL, process, (void *)"b"));
+	printf("create b %s %d\n", sucfail(pret), pret);
+	ret += (pret = pthread_join(th_a, &retval));
+	printf("join a %s %d\n", sucfail(pret), pret);
+	ret += (pret = pthread_join(th_b, &retval));
+	printf("join b %s %d\n", sucfail(pret), pret);
+	return ret;
 }
